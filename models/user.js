d24c98b7ed30ed9a5f9323f4ea2aede0ff0c03de
Falsin <greenldend@gmail.com>
2022-08-12 15:42:12 +0700

    changed model

--- models/user.js
+++ models/user.js
@@ -5,9 +5,12 @@ const Schema = mongoose.Schema;
 const UserSchema = new Schema({
   first_name: {type: String, required: true, maxLength: 100},
   last_name: {type: String, required: true, maxLength: 100},
-  login: {type: String, required: true, maxLength: 100},
+  username: {type: String, required: true, maxLength: 100},
   password: {type: String, required: true},
-  photo: {data: Buffer, contentType: String},
+  photo: {
+    data: Buffer, 
+    contentType: String,
+  },
   friends: {type: Array},
   incoming_friends_requests: {type: Array},
   outcoming_friends_requests: {type: Array},
