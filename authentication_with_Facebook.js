a8d01589cba4ca803a45b2b17a76583eba07fc1c
Falsin <greenldend@gmail.com>
2022-09-26 00:29:17 +0700

    s

--- authentication_with_Facebook.js
+++ authentication_with_Facebook.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 
 const passport = require("passport");
 const FacebookStrategy = require('passport-facebook');
+const User = require("./models/user");
 
 passport.use(new FacebookStrategy({
   clientID: process.env.FACEBOOK_CLIENT_ID,
@@ -10,19 +11,19 @@ passport.use(new FacebookStrategy({
   callbackURL: '/oauth2/redirect/facebook',
   state: true
 }, function verify(accessToken, refreshToken, profile, cb) {
-  db.get('SELECT * FROM federated_credentials WHERE provider = ? AND subject = ?', [
+  User.get('SELECT * FROM federated_credentials WHERE provider = ? AND subject = ?', [
     'https://www.facebook.com',
     profile.id
   ], function(err, row) {
     if (err) { return cb(err); }
     if (!row) {
-      db.run('INSERT INTO users (name) VALUES (?)', [
+      User.run('INSERT INTO users (name) VALUES (?)', [
         profile.displayName
       ], function(err) {
         if (err) { return cb(err); }
 
         var id = this.lastID;
-        db.run('INSERT INTO federated_credentials (user_id, provider, subject) VALUES (?, ?, ?)', [
+        User.run('INSERT INTO federated_credentials (user_id, provider, subject) VALUES (?, ?, ?)', [
           id,
           'https://www.facebook.com',
           profile.id
@@ -36,7 +37,7 @@ passport.use(new FacebookStrategy({
         });
       });
     } else {
-      db.get('SELECT * FROM users WHERE id = ?', [ row.user_id ], function(err, row) {
+      User.get('SELECT * FROM users WHERE id = ?', [ row.user_id ], function(err, row) {
         if (err) { return cb(err); }
         if (!row) { return cb(null, false); }
         return cb(null, row);
