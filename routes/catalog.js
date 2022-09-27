fb9cb8983697409f10c00fe40689885b4bbe956d
Falsin <greenldend@gmail.com>
2022-09-28 01:00:08 +0700

    s

--- routes/catalog.js
+++ routes/catalog.js
@@ -52,13 +52,13 @@ router.get("/login/facebook", passportForFacebook.authenticate("facebook"));
   res.redirect("https://localhost:8080")
 }); */
 
-/* router.get('/oauth2/redirect/facebook', passportForFacebook.authenticate('facebook', {
+router.get('/oauth2/redirect/facebook', passportForFacebook.authenticate('facebook', {
   successRedirect: 'https://localhost:8080',
   failureRedirect: '/error'
 })
-); */
+);
 
-router.get('/oauth2/redirect/facebook', passportForFacebook.authenticate('facebook'));
+//router.get('/oauth2/redirect/facebook', passportForFacebook.authenticate('facebook'));
 
 function createUserObject(obj) {
   const { 
