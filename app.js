75598e68af3683320a0e0d4f201d5d28bd8b4953
Falsin <greenldend@gmail.com>
2022-09-27 21:48:19 +0700

    s

--- app.js
+++ app.js
@@ -65,12 +65,13 @@ app.use(cookieSession({
 
 app.use(compression());
 
+app.use(cookieParser());
+
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', router);
