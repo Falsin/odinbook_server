d941d436d1b9fab23e71011d6d7e33eea7e20667
Falsin <greenldend@gmail.com>
2022-09-28 00:57:29 +0700

    s

--- app.js
+++ app.js
@@ -101,7 +101,7 @@ app.use(function(req, res, next) {
 // error handler
 app.use(function(err, req, res, next) {
   console.log('fuck')
-  console.log(err.message)
+  console.log(err)
 /*   console.log('fuck')
   console.log(err) */
   // set locals, only providing error in development
