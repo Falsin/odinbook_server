e72095dd243d653b14b222086d71112d5fa4734f
Falsin <greenldend@gmail.com>
2022-09-29 19:04:43 +0700

    изменил путь к user model

--- passport/authWithFacebook.js
+++ passport/authWithFacebook.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const FacebookStrategy = require('passport-facebook');
-const User = require("./models/user");
+const User = require("../models/user");
 
 const facebook = new FacebookStrategy({
   clientID: process.env.FACEBOOK_CLIENT_ID,
