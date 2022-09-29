edce1b46a8fd68582fd2cf9d2c46f350c70e8b74
Falsin <greenldend@gmail.com>
2022-09-29 17:29:40 +0700

    вывел local stategy в отдельный модуль

---
+++ passport/authLocal.js
@@ -0,0 +1,26 @@
+const LocalStrategy = require("passport-local");
+const bcrypt = require('bcrypt');
+
+const User = require("./models/user");
+
+const local = new LocalStrategy((username, password, cb) => {
+  User.findOne({username: username}, (err, user) => {
+    if (err) {
+      return cb(err)
+    }
+
+    if (!user) {
+      return cb(null, false, {message: 'Incorrect username'})
+    } 
+    
+    bcrypt.compare(password, user.password, (err, result) => {
+      if (result) {
+        return cb(null, user);
+      } else {
+        return cb(null, false, {message: 'Incorrect password.'})
+      }
+    })
+  })
+});
+
+module.exports = local;
