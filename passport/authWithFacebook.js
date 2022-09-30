a3d7323c30adbd62000a0de256b4d145251e113a
Falsin <greenldend@gmail.com>
2022-09-30 18:54:30 +0700

    s

--- passport/authWithFacebook.js
+++ passport/authWithFacebook.js
@@ -5,6 +5,10 @@ const path = require('path');
 const FacebookStrategy = require('passport-facebook');
 const User = require("../models/user");
 
+const filePath = path.resolve(__dirname, "../public/images/user.jpeg");
+const fileContent = fs.readFileSync(filePath);
+const contentType = path.extname(filePath);
+
 const facebook = new FacebookStrategy({
   clientID: process.env.FACEBOOK_CLIENT_ID,
   clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
@@ -12,16 +16,8 @@ const facebook = new FacebookStrategy({
   }, async function verify(accessToken, refreshToken, profile, cb) {
     const currentUser = await User.findOne({facebookId: profile.id})
 
-    console.log('fuck')
 
     if(!currentUser) {
-/*       const filePath = path.resolve(__dirname, "../public/images/user.jpeg");
-      const fileContent = fs.readFileSync(filePath);
-      const contentType = path.extname(filePath); */
-
-      console.log(fileContent)
-      console.log(contentType)
-
       new User({
         first_name: profile.displayName.split(" ")[0],
         last_name: profile.displayName.split(" ")[1],
@@ -29,13 +25,11 @@ const facebook = new FacebookStrategy({
         facebookId: profile.id,
         password: "null",
         birth_date: new Date().toLocaleString(),
-/*         photo: {
+        photo: {
           data: fileContent,
           contentType: "image/" + contentType.slice(1)
-        } */
+        }
       }).save((err, user) => {
-        console.log('end')
-        console.log(err)
         return cb(null, user);
       })
     }
