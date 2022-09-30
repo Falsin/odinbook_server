ffe0875d9a849007a59dd7e0d0309075bc07c594
Falsin <greenldend@gmail.com>
2022-09-30 14:54:23 +0700

    добавил возможность считывать и устанавливать изображение по умолчанию при регистрации через facebook

--- passport/authWithFacebook.js
+++ passport/authWithFacebook.js
@@ -1,9 +1,10 @@
 require('dotenv').config();
+const fs = require('fs');
+const path = require('path');
+
 const FacebookStrategy = require('passport-facebook');
 const User = require("../models/user");
 
-let fs = require('fs');
-let fileContent = fs.readFileSync("../public/images/user.jpeg");
 
 const facebook = new FacebookStrategy({
   clientID: process.env.FACEBOOK_CLIENT_ID,
@@ -11,16 +12,23 @@ const facebook = new FacebookStrategy({
   callbackURL: process.env.SERVER_URL + 'oauth2/redirect/facebook',
   }, async function verify(accessToken, refreshToken, profile, cb) {
     const currentUser = await User.findOne({facebookId: profile.id})
-    console.log(fileContent)
 
     if(!currentUser) {
+      const filePath = path.resolve(__dirname, "../public/images/user.jpeg");
+      const fileContent = fs.readFileSync(filePath);
+      const contentType = path.extname(filePath);
+
       new User({
         first_name: profile.displayName.split(" ")[0],
         last_name: profile.displayName.split(" ")[1],
         username: profile.displayName,
         facebookId: profile.id,
         password: "",
-        birth_date: new Date().toLocaleString()
+        birth_date: new Date().toLocaleString(),
+        photo: {
+          data: fileContent,
+          contentType: "image/" + contentType.slice(1)
+        }
       }).save((err, user) => {
         return cb(null, user);
       })
