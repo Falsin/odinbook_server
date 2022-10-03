2f3246eb3b63918ef7c9b022e92511516317a36e
Falsin <greenldend@gmail.com>
2022-10-03 07:09:07 +0700

    s

--- passport/authWithFacebook.js
+++ passport/authWithFacebook.js
@@ -16,6 +16,8 @@ const facebook = new FacebookStrategy({
   }, async function verify(accessToken, refreshToken, profile, cb) {
     const currentUser = await User.findOne({facebookId: profile.id})
 
+    console.log('hello1')
+
     if(!currentUser) {
       return new User({
         first_name: profile.displayName.split(" ")[0],
@@ -29,6 +31,7 @@ const facebook = new FacebookStrategy({
           contentType: "image/" + contentType.slice(1)
         }
       }).save((err, user) => {
+        console.log('hello2')
         return cb(null, user);
       })
     }
