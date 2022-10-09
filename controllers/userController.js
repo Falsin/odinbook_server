2df2cf98c18e12c22db05e27df34915bcaf7e8af
Falsin <greenldend@gmail.com>
2022-10-09 08:11:57 +0700

    добавил контроллер delete

--- controllers/userController.js
+++ controllers/userController.js
@@ -131,6 +131,20 @@ exports.friend_put = async (req, res, next) => {
   res.json(currentUser);
 }
 
+exports.friend_delete = async (req, res, next) => {
+  let currentUser = await User.findById(req.user._id);
+  let friendUser = await User.findById(req.body._id);
+
+  if (currentUser.friends.includes(req.body._id)) {
+    await deleteAndAddUser(currentUser, "incoming_friends_requests", req.body._id);
+    await deleteAndAddUser(friendUser, "outcoming_friends_requests", req.body._id);
+  } else {
+    currentUser = null;
+  }
+
+  res.json(currentUser);
+}
+
 async function deleteAndAddFriend (nameArray, friendId) {
   const id = this[nameArray].indexOf(friendId);
   this[nameArray].splice(id, 1);
@@ -138,14 +152,21 @@ async function deleteAndAddFriend (nameArray, friendId) {
   addUserToArray.call(this, "friends", friendId);
 }
 
-async function addUserToArray (array, friendId) {
-  this[array].push(friendId);
+async function addUserToArray (nameArray, friendId) {
+  this[nameArray].push(friendId);
   this.save();
 }
 
 async function getPopulateListUsers (userId, array) {
   const populatedUser = await User.findById(userId).populate(array);
   return populatedUser[array];
+}
+
+async function deleteAndAddUser (nameArray, friendId) {
+  const id = this.friends.indexOf(friendId);
+  this.friends.splice(id, 1);
+
+  addUserToArray.call(this, nameArray, friendId);
 }
 
 /* async function deleteAndAddFriend (currentUser, nameArray, friendId) {
