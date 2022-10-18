b1775411c91f2fb88c7b2a15b0fcee29ad190cf9
Falsin <greenldend@gmail.com>
2022-10-18 10:16:38 +0700

    s

--- controllers/postController.js
+++ controllers/postController.js
@@ -12,6 +12,8 @@ exports.post_post = [
   async (req, res, next) => {
     const errorArray = validationResult(req).array;
 
+    console.log(errorArray);
+
     if (errorArray.length == 2) {
       return res.json(false)
     }
@@ -51,58 +53,5 @@ async function getFriendNews(sourceArray, userId) {
   const friendPosts = [];
   friendPosts.push(...await Post.find({author: userId}).populate("author"));
 
-  /* for (const item of array) {
-    friendPosts.push(await Post.find({author: item}).populate("author"));
-  } */
   sourceArray.push(...friendPosts);
 }
-
-/* exports.posts_get = async (req, res, next) => {
-  let postArray = [];
-
-  try {
-    const currentUser = await User.findById(req.user._id);
-
-    const array = [[currentUser._id], currentUser.friends, currentUser.outcoming_friends_requests];
-    await Promise.all(array.map(async (elem) => getFriendNews(postArray, elem)))
-
-    postArray.sort((a, b) => a.date.getTime() - b.date.getTime());
-  } finally {
-    return res.json(postArray)
-  }
-}
-
-async function getFriendNews(sourceArray, array) {
-  const friendPosts = [];
-
-  for (const item of array) {
-    friendPosts.push(...await Post.find({author: item}).populate("author"));
-  }
-  sourceArray.push(...friendPosts);
-} */
-
-/* exports.posts_get = async (req, res, next) => {
-  let postArray = [];
-
-  try {
-    const currentUser = await User.findById(req.user._id);
-
-    let currentUserPosts = await Post.find({author: currentUser._id}).populate("author");
-    postArray.push(...currentUserPosts);
-
-    for (const item of currentUser.friends) {
-      const friendPosts = await Post.find({author: item}).populate("author");
-      postArray.push(...friendPosts);
-    }
-
-    for (const item of currentUser.outcoming_friends_requests) {
-      const outcomingFriendPosts = await Post.find({author: item}).populate("author");
-      postArray.push(...outcomingFriendPosts);
-    }
-
-    postArray.sort((a, b) => a.date.getTime() - b.date.getTime());
-  } finally {
-    return res.json(postArray)
-  }
-} */
-//19 строчек
